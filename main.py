7064c5312f0c4e5516a7274312dfbe5ea42d8469
Tony Tong <tszhin07@hotmail.com>
2018-12-30 01:50:48 +0800

    Testing out Heroku's config option

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ def error_callback(bot, update, error):
 
 def main():
     # Create the EventHandler and pass it your bot's token.
-    updater = Updater("725959580:AAHBI0I0A023e6sXw1ErIPYWPU86iBLEhp4")
+    updater = Updater(TELEGRAM_TOKEN)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
